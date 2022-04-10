fb77eedddd82a57d0f844679e323427317881fce
ryan <ryan.williams93@aol.com>
2022-04-10 14:35:49 -0500

    established typeDefs and routed typedefs/resolvers in index.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -15,10 +15,36 @@ const typeDefs = gql`
 
     type Reaction {
         _id: ID
-        reactionBody: String
+    }
+
+    type DeathFact {
+        _id: ID
+        deathtext
         createdAt: String
         username: String
+        reactionCount: Int
+        reactions: [Reaction]
+    }
+
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+        deathFacts(username: String!): [DeathFact]
+        deathFact(_id: ID!): DeathFact
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addDeathFact(deathtext: String!): DeathFact
+        addReaction(deathFactId: ID!, reactionBody: String!): DeathFact
     }
     
-
+    type Auth {
+        token: ID!
+        user: User
+    }
 `;
+
+module.exports = typeDefs;
