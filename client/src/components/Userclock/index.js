dd08369fc31b83450207991abf5d5faf1ce320bd
GunnySensei <gunnerwagoner@hotmail.com>
2022-04-12 21:01:04 -0500

    hardcoded formatTime added

--- client/src/components/Userclock/index.js
+++ client/src/components/Userclock/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 const username = "placeholder";
-const time = Date.now();
+
+const timeLeft = require("../../utils/formatTime");
 
 const Userclock = () => {
   return (
     <div className="userclock-container">
       <div className="user-info-container">
         <h1 className="username-h1">{username}</h1>
-        <h2 className="user-time-counter">You have {time} left.</h2>
+        <h2 className="user-time-counter">
+          You have {timeLeft.hoursLeft} hours, {timeLeft.daysLeft} days,{" "}
+          {timeLeft.weeksLeft} weeks, {timeLeft.monthsLeft} months, and{" "}
+          {timeLeft.yearsLeft} years left.
+        </h2>
       </div>
     </div>
   );
---
+++ client/src/utils/formatTime.js
@@ -0,0 +1,25 @@
+let timeLeft = {};
+
+const getTimeLeft = () => {
+  let birthday = new Date(94, 12, 1, 13, 30, 30, 0);
+  let deathDay = new Date(2072, 12, 1, 13, 30, 30, 0);
+  let totalLife = deathDay - birthday;
+  let timeNow = new Date();
+  let secondsLeft = totalLife - timeNow;
+  console.log(totalLife);
+  timeLeft = {
+    hoursLeft: Math.floor(secondsLeft / 3600000),
+    daysLeft: Math.floor(secondsLeft / 86400000),
+    weeksLeft: Math.floor(secondsLeft / 604800000),
+    monthsLeft: Math.floor(secondsLeft / 2629743000),
+    yearsLeft: Math.floor(secondsLeft / 31556926000),
+  };
+
+  //take the date of birth
+  // const birthDate = new Date();
+  //calculate time of death from dob
+  //calculate time left from today until death day
+};
+getTimeLeft();
+
+module.exports = timeLeft;
