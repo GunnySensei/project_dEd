82866f30bcd4b6e437811e612183eb6d84e605c9
diirtydog <andrew.derek.wales@gmail.com>
2022-04-12 20:31:01 -0500

    added all the mutations on the front end for the forn*#ator$

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,57 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_DEATHFACT = gql`
+    mutation addDeathFact($deathText: String!) {
+        addDeathFact(deathText: $deathText) {
+            _id
+            deathText
+            createdAt
+            username
+            reactions {
+                _id
+            }
+        }
+    }
+`;
+
+export const ADD_REACTION = gql`
+    mutation addReaction($deathFactId: ID!, $reactionBody: String!) {
+        addReaction(deathFactId: $deathFactId, reactionBody: $reactionBody) {
+            _id
+            reactionCount
+            reactions {
+                _id
+                reactions {
+                    _id
+                    reactionBody
+                    createdAt
+                    username
+                }
+            }
+        }
+    }
+`;
