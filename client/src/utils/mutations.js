f61555961ed6666792884140fdc668331a530f1d
Kevin <kevin.j.wilkerson@gmail.com>
2022-04-14 20:12:54 -0500

    added sex at birth and birthday to sign up form

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,59 +1,71 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-    mutation login($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-            }
-        }
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
+  }
 `;
 
 export const ADD_USER = gql`
-    mutation addUser($username: String!, $email: String!, $password: String!, $birthday: String, $sex: String!) {
-        addUser(username: $username, email: $email, password: $password, birthday: $birthday, sex: $sex) {
-            token
-            user {
-                _id
-                username
-                birthday
-                sex
-            }
-        }
+  mutation addUser(
+    $username: String!
+    $email: String!
+    $password: String!
+    $birthday: String!
+    $sex: String!
+  ) {
+    addUser(
+      username: $username
+      email: $email
+      password: $password
+      birthday: $birthday
+      sex: $sex
+    ) {
+      token
+      user {
+        _id
+        username
+        birthday
+        sex
+      }
     }
+  }
 `;
 
 export const ADD_DEATHFACT = gql`
-    mutation addDeathFact($deathText: String!) {
-        addDeathFact(deathText: $deathText) {
-            _id
-            deathText
-            createdAt
-            username
-            reactions {
-                _id
-            }
-        }
+  mutation addDeathFact($deathText: String!) {
+    addDeathFact(deathText: $deathText) {
+      _id
+      deathText
+      createdAt
+      username
+      reactions {
+        _id
+      }
     }
+  }
 `;
 
 export const ADD_REACTION = gql`
-    mutation addReaction($deathFactId: ID!, $reactionBody: String!) {
-        addReaction(deathFactId: $deathFactId, reactionBody: $reactionBody) {
-            _id
-            reactionCount
-            reactions {
-                _id
-                reactions {
-                    _id
-                    reactionBody
-                    createdAt
-                    username
-                }
-            }
+  mutation addReaction($deathFactId: ID!, $reactionBody: String!) {
+    addReaction(deathFactId: $deathFactId, reactionBody: $reactionBody) {
+      _id
+      reactionCount
+      reactions {
+        _id
+        reactions {
+          _id
+          reactionBody
+          createdAt
+          username
         }
+      }
     }
+  }
 `;
