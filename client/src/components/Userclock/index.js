fdb0325e406fd84bb59a8609f4b8155133e678d5
GunnySensei <gunnerwagoner@hotmail.com>
2022-04-14 20:42:04 -0500

    integrated format time and userClock

--- client/src/components/Userclock/index.js
+++ client/src/components/Userclock/index.js
@@ -1,13 +1,18 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
-const username = "placeholder";
-
-const timeLeft = require("../../utils/formatTime");
+import getTimeLeft from "../../utils/formatTime";
+import { QUERY_ME } from "../../utils/queries";
 
 const Userclock = () => {
+  const { data: userData } = useQuery(QUERY_ME);
+
+  let dateString = userData.me.birthday;
+  let timeLeft = getTimeLeft(dateString);
+
   return (
     <div className="userclock-container">
       <div className="user-info-container">
-        <h1 className="username-h1">{username}</h1>
+        <h1 className="username-h1">{userData.me.username}</h1>
         <h2 className="user-time-counter">
           You have {timeLeft.hoursLeft} hours, {timeLeft.daysLeft} days,{" "}
           {timeLeft.weeksLeft} weeks, {timeLeft.monthsLeft} months, and{" "}
--- client/src/utils/formatTime.js
+++ client/src/utils/formatTime.js
@@ -1,22 +1,13 @@
-let timeLeft = {};
-
 //!take birthday from user once logged in
 //parse birthday from user token
 
-// take string from user -> take first 4 digits = year, next 2 digits = month, last 2 digits = day
-let User = {
-  dateString: "2000-09-10",
-};
-
-let dateString = User.dateString;
-let parsedDateString = dateString.split("-");
-const ParsedDateCharacter = {
-  year: parseInt(parsedDateString[0]),
-  month: parseInt(parsedDateString[1]),
-  day: parseInt(parsedDateString[2]),
-};
-
-const getTimeLeft = (ParsedDateCharacter) => {
+const getTimeLeft = (dateString) => {
+  let parsedDateString = dateString.split("-");
+  const ParsedDateCharacter = {
+    year: parseInt(parsedDateString[0]),
+    month: parseInt(parsedDateString[1]),
+    day: parseInt(parsedDateString[2]),
+  };
   let birthday = new Date(
     ParsedDateCharacter.year,
     ParsedDateCharacter.month,
@@ -30,24 +21,19 @@ const getTimeLeft = (ParsedDateCharacter) => {
   let totalLife = deathDay - birthday;
   let timeNow = new Date();
   let secondsLeft = totalLife - timeNow;
-  timeLeft = {
+  let timeLeft = {
     hoursLeft: Math.floor(secondsLeft / 3600000),
     daysLeft: Math.floor(secondsLeft / 86400000),
     weeksLeft: Math.floor(secondsLeft / 604800000),
     monthsLeft: Math.floor(secondsLeft / 2629743000),
     yearsLeft: Math.floor(secondsLeft / 31556926000),
   };
+  return timeLeft;
 
   //take the date of birth
   // const birthDate = new Date();
   //calculate time of death from dob
   //calculate time left from today until death day
 };
-console.log(
-  ParsedDateCharacter.year,
-  ParsedDateCharacter.month,
-  ParsedDateCharacter.day
-);
-getTimeLeft(ParsedDateCharacter);
 
-module.exports = timeLeft;
+module.exports = getTimeLeft;
