53f05bca7efc64852d5c861ce5e30b90ad080def
GunnySensei <gunnerwagoner@hotmail.com>
2022-04-14 20:48:29 -0500

    userClock

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,4 +1,4 @@
-import { gql } from 'apollo/client';
+import { gql } from "apollo/client";
 
 export const QUERY_ME_BASIC = gql`
     {
@@ -14,79 +14,79 @@ export const QUERY_ME_BASIC = gql`
 `;
 
 export const QUERY_ME = gql`
-    {
-        me {
-            _id
-            username
-            email
-            birthday
-            sex
-            deathFacts {
-                _id
-                deathText
-                createdAt
-                reactionCount
-                reactions {
-                    _id
-                    createdAt
-                    reactionBody
-                    username
-                }
-            }
+  {
+    me {
+      _id
+      username
+      email
+      birthday
+      sex
+      deathFacts {
+        _id
+        deathText
+        createdAt
+        reactionCount
+        reactions {
+          _id
+          createdAt
+          reactionBody
+          username
         }
+      }
     }
+  }
 `;
 
 export const QUERY_USER = gql`
-    query User($id: ID!) {
-        user(id: $id) {
-            _id
-            username
-            email
-            birthday
-            sex
-            deathFacts {
-                _id
-                deathText
-                createdAt
-                reactionCount
-            }
-        }
+  query User($id: ID!) {
+    user(id: $id) {
+      _id
+      username
+      email
+      birthday
+      sex
+      deathFacts {
+        _id
+        deathText
+        createdAt
+        reactionCount
+      }
     }
+  }
 `;
 
 export const QUERY_DEATHFACTS = gql`
-    query deathFacts($username: String) {
-        deathFacts(username: $username) {
-            _id
-            deathText
-            createdAt
-            username
-            reactionCount
-            reaction {
-                _id
-                createdAt
-                username
-                reactionBody
-            }
-        }
+  query deathFacts($username: String) {
+    deathFacts(username: $username) {
+      _id
+      deathText
+      createdAt
+      username
+      reactionCount
+      reaction {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
     }
+  }
 `;
 
 export const QUERY_DEATHFACT = gql`
-    query deathFact($_id: ID!) {
-        deathFact(_id: $_id) {
-            _id
-            deathText
-            createdAt
-            username
-            reactionCount
-            reaction {
-                _id
-                createdAt
-                username
-                reactionBody
-            }
-        }
+  query deathFact($_id: ID!) {
+    deathFact(_id: $_id) {
+      _id
+      deathText
+      createdAt
+      username
+      reactionCount
+      reaction {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
     }
+  }
 `;
