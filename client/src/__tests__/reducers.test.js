ca95ff69028da0f45b2fff137dc1150d20bc7390
ryan <ryan.williams93@aol.com>
2022-04-17 13:27:31 -0500

    added categories test, and globalstate file

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -4,13 +4,15 @@ import { reducer } from '../utils/reducers';
 // import actions
 import {
     UPDATE_DONATIONS,
+    UPDATE_D_CAT,
     UPDATE_CURRENT_DONATION
 } from '../utils/actions';
 
 // sample of what our global state will look like
 const initialState = {
     donations: [],
-    currentDonation: 1.00,
+    categories: [{ price: 1.00 }],
+    currentDonation: '1',
 };
 
 test('UPDATE_DONATIONS', () => {
@@ -23,12 +25,22 @@ test('UPDATE_DONATIONS', () => {
     expect(initialState.donations.length).toBe(0);
 });
 
+test('UPDATE_CATEGORIES', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_D_CAT,
+        categories: [{}, {}]
+    });
+
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+});
+
 test('UPDATE_CURRENT_DONATION', () => {
     let newState = reducer(initialState, {
         type: UPDATE_CURRENT_DONATION,
-        currentDonation: 2.00
+        currentDonation: '2'
     });
   
-    expect(newState.currentDonation).toBe(2.00);
-    expect(initialState.currentDonation).toBe(1.00);
+    expect(newState.currentDonation).toBe('2');
+    expect(initialState.currentDonation).toBe('1');
   });
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,6 +1,7 @@
 import { useReducer } from "react";
 import {
     UPDATE_DONATIONS,
+    UPDATE_D_CAT,
     UPDATE_CURRENT_DONATION
 } from "./actions";
   
@@ -10,6 +11,12 @@ export const reducer = (state, action) => {
             return {
                 ...state,
                 donations: [...action.donations],
+            };
+        
+        case UPDATE_D_CAT:
+            return {
+                ...state,
+                categories: [...action.categories]
             };
 
         case UPDATE_CURRENT_DONATION:
