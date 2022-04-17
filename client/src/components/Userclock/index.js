1c32ecfd003207d8fe94df448ae5969cd5b30b46
Kevin <kevin.j.wilkerson@gmail.com>
2022-04-17 14:53:04 -0500

    bug fixes

--- client/src/components/Userclock/index.js
+++ client/src/components/Userclock/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Auth from "../../utils/auth";
+// import Auth from "../../utils/auth";
 import { useQuery } from "@apollo/client";
 import { QUERY_ME } from "../../utils/queries";
 
